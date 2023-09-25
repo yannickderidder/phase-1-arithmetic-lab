65fbb1ffd951b477e313503aad64572b600fdf44
+ <+>
2023-09-25 22:20:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let multiply = 62;
+let num1;
+let num2;
+for (num1 = 1; num1 <= multiply; num1++) {
+    if (multiply % num1 === 0){
+        num2 = multiply / num1;
+        break;
+    }
+}
+console.log("num1: " + num1);
+console.log("num2: " + num2);
 
+var random
+var random = Math.ceil(Math.random()
+* Number.MAX_SAFE_INTEGER);
+
+var mod = 4;
+var num3 = 20;
+var num4 = 8;
+var remainder = num3 % num4;
+
+var max
+var max = Math.max(20, 10, 15, 5, 20, 18);
 // Write your code here
